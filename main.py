315fe5e439098f44f0dfd780e2ca831a1f816842
mahdihammi <mahdihammi12@gmail.com>
2024-11-01 15:07:41 +0000

    Data transformation added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from mlProject import logger
 from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from mlProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 
@@ -13,6 +14,9 @@ except Exception as e:
     logger.exception(e)
     raise e
 
+
+print("\n================================")
+##############################################################################################
 STAGE_NAME = "Data Validation stage"
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
@@ -22,3 +26,15 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+print("\n================================")
+###########################################################################################
+STAGE_NAME = "Data Transformation Stage"
+try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataTransformationTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
